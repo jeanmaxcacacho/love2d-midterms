28f2b746d08e1581ee4961af755f581d46189201
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-30 14:40:59 +0800

    Entity class init

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
 function love.load()
 	Object = require("lib.classic")
+
+	require("src.entity")
+	player = Entity(
+		"assets/panda.png",
+		100,
+		100
+	)
 end
 
 function love.update(dt)
@@ -7,5 +14,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
+	player:draw()
 end
---
+++ src/entity.lua
@@ -0,0 +1,12 @@
+Entity = Object:extend()
+
+function Entity:new(sprite, x, y)
+	-- sprite will be a string containing the path to the img
+	self.sprite = love.graphics.newImage(sprite)
+	self.x = x
+	self.y = y
+end
+
+function Entity:draw()
+	love.graphics.draw(self.sprite, self.x, self.y)
+end
